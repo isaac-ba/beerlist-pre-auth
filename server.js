bd6f9e5e357687eb6f9f00a3f99495b52f77c201
Isaac BA <iba.burton@gmail.com>
2016-12-18 11:40:47 +0200

    third commit

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ mongoose.connect('mongodb://localhost/beers');
 
 var Beer = require("./models/BeerModel");
 var Review = require("./models/ReviewModel");
-var User = require('./models/UserModel')
-var LocalStrategy = require('passport-local').Strategy;
-
+var User = require("./models/UserModel");
 
 var app = express();
 
@@ -18,6 +16,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+//------------------------
+// For authentication:
 var passport = require('passport');
 var expressSession = require('express-session');
 
@@ -25,6 +25,7 @@ app.use(expressSession({ secret: 'mySecretKey' }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+//------------------------
 
 app.get('/beers', function (req, res) {
   Beer.find(function (error, beers) {
@@ -83,18 +84,6 @@ app.post('/beers/:id/reviews', function(req, res, next) {
   });
 });
 
-
-passport.use('register', new LocalStrategy(function (username, password, done) {
-  var user = {
-    username: username,
-    password: password
-  }
-
-  console.log(user);
-
-  done(null, user);
-}));
-
 app.delete('/beers/:beer/reviews/:review', function(req, res, next) {
   Beer.findById(req.params.beer, function (err, beer) {
     for (var i = 0; i < beer.reviews.length; i ++) {
@@ -109,9 +98,32 @@ app.delete('/beers/:beer/reviews/:review', function(req, res, next) {
   });
 });
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use('register', new LocalStrategy(function (username, password, done) {
+  var user = {
+    username: username,
+    password: password
+  }
+
+  console.log(user);
+
+  done(null, user);
+}));
+
+app.get('/currentUser', function (req, res) {
+  res.send(req.user);
+});
+
 app.post('/register', passport.authenticate('register'), function (req, res) {
   res.json(req.user);
 });
-
 
 app.listen(8000);
